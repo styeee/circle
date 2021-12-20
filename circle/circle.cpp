73a3ae34fe843d176c60d02c7411b0f67db65d89
Станислав Шубин <sti.99@mail.ru>
2021-12-20 23:19:03 +0300

    Добавьте файлы проекта.

---
+++ circle/circle.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(size_t y=0;y<32;y++)
+    {
+        for(size_t x=0;x<32;x++)
+        {
+            const size_t xx=x-16;
+            const size_t yy=y-16;
+
+            if((xx*xx+yy*yy)>128)
+                cout<<' '<<' ';
+            else
+                cout<<' '<<'*';
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
